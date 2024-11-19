bc2cc11233c25ca212d758c3d2874298347b1aed
Sebastienlumuna <sebastienlumuna8@gmail.com>
2024-11-19 16:34:49 +0100

    premier commit du projet

---
+++ template isipa kom/js/main.js
@@ -0,0 +1,20 @@
+/**
+* Template Name: iLanding
+* Template URL: https://bootstrapmade.com/ilanding-bootstrap-landing-page-template/
+* Updated: Oct 28 2024 with Bootstrap v5.3.3
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+
+(function() {
+ 
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
+})();
